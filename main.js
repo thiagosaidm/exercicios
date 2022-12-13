6579f616974b8d654265e637e1fbc54baf5c1c56
thiagosaidm <thiag.said@gmail.com>
2022-12-13 19:49:58 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+function converterParaCelsius(temp) {
+    var celcius = ((temp - 32)*5)/9;
+
+    console.log(`a temperatura de ${temp}F é igual a ${celcius}`);
+}
+
+
+
+converterParaCelsius(90);
+
+//////
+
+
+
+
+function calcularSomaMedia(a,b,c) {
+    let soma = a+b+c;
+
+    let media = soma / 3;
+
+
+    console.log(media)
+}
+
+calcularSomaMedia(5,6,7)
+
+
+//////
+
+
+
