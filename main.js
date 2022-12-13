4badf6c16395e7c6c4da0761143dc6d07b5b3c80
thiagosaidm <thiag.said@gmail.com>
2022-12-13 20:04:51 -0300

    "#"

--- main.js
+++ main.js
@@ -28,5 +28,26 @@ calcularSomaMedia(5,6,7)
 
 //////
 
+/*var primeiro = window.prompt("digite um numero");
+var segundo = window.prompt("digite outro numero")
+
+if (primeiro + segundo > 100) {
+    var resultado = (parseInt(primeiro)) + (parseInt(segundo));
+    console.log(`a soma deu ${resultado}, que é maior que 100`)
+}else{
+    console.log("a soma não é maior que 100")
+}
 
 
+*//////
+
+function personagemMorreu(dano, saude) {
+    if (dano >= saude) {
+            console.log(1);
+    } else {
+        console.log(0)
+    }
+}
+
+
+personagemMorreu(10,100)
