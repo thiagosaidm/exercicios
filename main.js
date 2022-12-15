469bff944dcfab14d760aeabd805276fed5aea5c
thiagosaidm <thiag.said@gmail.com>
2022-12-15 20:49:42 -0300

    "#"

--- main.js
+++ main.js
@@ -39,21 +39,31 @@ if (primeiro + segundo > 100) {
 }
 
 
-*//////
+*/
+
+//////
+
+/*
 
 function personagemMorreu(dano, saude) {
     if (dano >= saude) {
-            console.log(1);
+            alert("E MORREU");
     } else {
-        console.log(0)
+        alert("VEM TRANQUILO")
     }
 }
 
 
-personagemMorreu(10,100)
+personagemMorreu(parseInt(window.prompt("dano")), (parseInt(window.prompt("life"))))
 
-//
+*/
 
+
+//////
+
+
+
+/*
 
 function limitaPosicao(posicao){
     if (posicao > 100 && posicao < 0) {
@@ -63,19 +73,97 @@ function limitaPosicao(posicao){
     }
 }
 
-///
+*/
+
+////////
 
 
 
+
+/*
 
 function MostrarTabuada(numero) {
     let tabuada = [];
     for (let i = 0; i < 10; i++) {
-        tabuada.push(`${numero} x ${i} =  ` + numero * i);
+        tabuada.push(` ${numero} x ${i} =  ` + numero * i,);
         
     }
 
-    console.log(tabuada)
+    document.write(tabuada)
 }
 
-MostrarTabuada(parseInt(window.prompt("digita ai um numero")))
+MostrarTabuada(parseInt(window.prompt("digite um numero")))
+
+
+
+*/
+
+//ORDENAR ARRAY
+
+/*
+function ordenarArray(array) {
+
+    let novoArrayOrd = array.sort( (a,b) =>{
+        return a - b;
+    })
+
+    console.log(novoArrayOrd)
+}
+
+
+ordenarArray([3,10,9])
+
+*/
+
+
+// URNA ELETRONICA
+
+/* 
+
+Uma eleição possui 2 candidatos a prefeito: o candidato José, do Partido da Felicidade e o candidato João, do Partido da Esperança. 
+Os eleitores votam nos candidatos pelo número do partido. 
+Para votar no candidato José do Partido da Felicidade, precisam digitar o número 1. 
+Para votar no candidato João, do Partido da Esperança, precisam digitar o número 2.
+
+Faça um programa que simule uma urna eletrônica. 
+Para cada eleitor, deve ser lido o número do seu título e em seguida o eleitor poderá digitar o seu voto. 
+A eleição é encerrada quando um valor negativo (menor do que 0) para o título do eleitor é digitado. 
+Informe quantos votos cada candidato teve e qual deles ganhou a eleição.
+
+*/
+
+
+let titulo, totalJose, totalJoao;
+
+totalJose = 0;
+totalJoao = 0;
+
+
+do {
+    alert("DIGITE O NUMERO DO SEU TITULO DE ELEITOR");
+    titulo = Number(prompt())
+    if (titulo != 0) {
+        alert("ESCOLHA O CANDIDATO: JOSÉ (1), JOÃO (2), NULO (0)")
+        var voto = Number(prompt());
+        switch (voto) {
+            case 1: totalJose++;
+            break;
+            case 2: totalJoao++;
+            break;
+            default : alert("VOTO NULO");
+        }
+        alert("OBRIGADO POR VOTAR!");
+    } 
+} while (titulo != 0);
+
+document.write(`RESULTADO=> TOTAL JOAO: ${totalJoao},  TOTAL JOSE: ${totalJose},   `)
+
+if (totalJose > totalJoao) {
+    console.log("José foi eleito.");
+}
+else if (totalJoao == totalJose) {
+    console.log("A eleição empatou, tirem no dado.");
+}
+else {
+    console.log("João foi eleito.");
+}
