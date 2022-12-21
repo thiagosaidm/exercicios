c3f00eae82e991f656ac14462aeac6f02d3e0f3b
thiagosaidm <thiag.said@gmail.com>
2022-12-20 22:57:34 -0300

    "@"

---
+++ ReadMe.md
@@ -0,0 +1,19 @@
+Escreva uma função que verifique se um número é par ou ímpar e retorne "par" ou "ímpar" respectivamente.
+
+Escreva uma função que receba uma string como argumento e retorne o número de vogais presentes na string.
+
+Escreva uma função que receba um número inteiro como argumento e retorne a soma de todos os números até o número fornecido. Por exemplo, se a função receber 5 como argumento, ela deverá retornar 15 (1 + 2 + 3 + 4 + 5).
+
+Escreva uma função que receba uma string como argumento e retorne a string invertida. Por exemplo, se a função receber "hello" como argumento, ela deverá retornar "olleh".
+
+Escreva uma função que verifique se uma string é um palíndromo (uma palavra que pode ser lida tanto da esquerda para a direita quanto da direita para a esquerda). Por exemplo, a string "racecar" é um palíndromo.
+
+Escreva uma função que calcule o fatorial de um número. O fatorial de um número n é o produto de todos os números inteiros menores ou iguais a n. Por exemplo, o fatorial de 5 é 5 * 4 * 3 * 2 * 1 = 120.
+
+Escreva uma função que receba um número inteiro como argumento e verifique se é um número primo. Um número primo é um número divisível apenas por 1 e por ele mesmo.
+
+Escreva uma função que receba dois números inteiros como argumento e retorne o máximo divisor comum (MDC) entre eles. O MDC é o maior número que divide ambos os números sem deixar resto.
+
+Escreva uma função que receba um array de números como argumento e retorne o maior número presente no array.
+
+Escreva uma função que receba um array de strings como argumento e retorne um novo array com as strings ordenadas em ordem alfabética.
--- main.js
+++ main.js
@@ -115,55 +115,147 @@ ordenarArray([3,10,9])
 
 */
 
+/*
+Escreva uma função que 
+verifique se um número é par ou ímpar e retorne "par" ou "ímpar" respectivamente.
+R:
+function EhparOuImpar(number) {
+    if (number % 2 == 0) {
+        console.log("É par")
+    } else {
+        console.log("É impar")
+    }
+}
 
-// URNA ELETRONICA
-
-/* 
-
-Uma eleição possui 2 candidatos a prefeito: o candidato José, do Partido da Felicidade e o candidato João, do Partido da Esperança. 
-Os eleitores votam nos candidatos pelo número do partido. 
-Para votar no candidato José do Partido da Felicidade, precisam digitar o número 1. 
-Para votar no candidato João, do Partido da Esperança, precisam digitar o número 2.
-
-Faça um programa que simule uma urna eletrônica. 
-Para cada eleitor, deve ser lido o número do seu título e em seguida o eleitor poderá digitar o seu voto. 
-A eleição é encerrada quando um valor negativo (menor do que 0) para o título do eleitor é digitado. 
-Informe quantos votos cada candidato teve e qual deles ganhou a eleição.
+EhparOuImpar(2)
 
 */
 
-
-let titulo, totalJose, totalJoao;
-
-totalJose = 0;
-totalJoao = 0;
+/* 
+Escreva uma função que receba uma string como argumento 
+e retorne o número de vogais presentes na string.
 
 
-do {
-    alert("DIGITE O NUMERO DO SEU TITULO DE ELEITOR");
-    titulo = Number(prompt())
-    if (titulo != 0) {
-        alert("ESCOLHA O CANDIDATO: JOSÉ (1), JOÃO (2), NULO (0)")
-        var voto = Number(prompt());
-        switch (voto) {
-            case 1: totalJose++;
-            break;
-            case 2: totalJoao++;
-            break;
-            default : alert("VOTO NULO");
-        }
-        alert("OBRIGADO POR VOTAR!");
-    } 
-} while (titulo != 0);
+pega a palavra
+lista as vogais
+separa a palavra em letras
+verifica se tem as vogais
+separa num array
+retorna o tamanho do array
 
-document.write(`RESULTADO=> TOTAL JOAO: ${totalJoao},  TOTAL JOSE: ${totalJose},   `)
 
-if (totalJose > totalJoao) {
-    console.log("José foi eleito.");
+*/
+
+function contarVogais(str) {
+    // Inicializamos uma variável para contar o número de vogais
+    let vogais = 0;
+  
+    // Convertemos a string em um array de caracteres
+    const caracteres = str.split('');
+  
+    // Iteramos pelo array de caracteres
+    for (let i = 0; i < caracteres.length; i++) {
+      // Verificamos se o caractere atual é uma vogal
+      if ('aeiouAEIOU'.indexOf(caracteres[i]) !== -1) {
+        // Se sim, incrementamos o contador de vogais
+        vogais++;
+      }
+    }
+  
+    // Retornamos o número de vogais
+    return vogais;
+  }
+
+  console.log(contarVogais("thiago"))
+
+
+  /*
+  
+  Escreva uma função que receba um número inteiro como argumento 
+  e retorne a soma de todos os números até o número fornecido. 
+  Por exemplo, se a função receber 5 como argumento, ela deverá retornar 15 (1 + 2 + 3 + 4 + 5).
+  
+  */
+
+  function somarTodos(number) {
+    
+    let soma = 0;
+    for (let i = 1; i <= number; i++) {
+        soma += i;
+        
+    }
+
+    return soma;
+  }
+
+  console.log(somarTodos(5))
+
+
+  /* 
+  Escreva uma função que receba uma string como argumento e retorne a string invertida. 
+  Por exemplo, se a função receber "hello" como argumento, ela deverá retornar "olleh".
+  
+  */
+
+
+  function reverterPalavra(palavra) {
+    let palavraSeparada = palavra.split("")
+    
+    let palavraInvertida = palavraSeparada.reverse()
+ 
+    let palavraJunta = palavraInvertida.join('')
+
+    return palavraJunta
+  }
+
+  console.log(reverterPalavra('atecubanos'))
+
+
+  /* 
+
+  Escreva uma função que verifique se uma string é um palíndromo 
+  (uma palavra que pode ser lida tanto da esquerda para a direita quanto da direita para a esquerda). 
+  Por exemplo, a string "racecar" é um palíndromo.
+
+
+  */
+
+  function ehPalindromo(palavra) {
+    let palavraSeparada = palavra.split('');
+
+    let palavraInvertida = palavraSeparada.reverse().join('');
+
+    if (palavra === palavraInvertida) {
+        console.log("é palindromo")
+    } else {
+        console.log("não é palindromo")
+    }
+
+    
+  }
+
+
+  ehPalindromo("appa");
+
+
+  /*
+
+Escreva uma função que calcule o fatorial de um número. 
+O fatorial de um número n é o produto de todos os números inteiros menores ou iguais a n. 
+Por exemplo, o fatorial de 5 é 5 * 4 * 3 * 2 * 1 = 120.
+
+pegar o numero e diminuir ele, colocando 
+
+  */
+
+function calcularFatorial(num) {
+    if (num) {
+        
+    } else {
+        
+    }
+        
+    }
 }
-else if (totalJoao == totalJose) {
-    console.log("A eleição empatou, tirem no dado.");
-}
-else {
-    console.log("João foi eleito.");
-}
+
+console.log(calcularFatorial(5))
